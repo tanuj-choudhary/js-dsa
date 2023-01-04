7a4b0711ac6b71a18c43bea0f8f0567ef15902f0
Tanuj Choudhary <tanujchoudhary001@gmail.com>
2023-01-04 19:55:53 +0530

    Solved array reduce problems

---
+++ array-reduce/code.js
@@ -0,0 +1,78 @@
+const inputThree = [
+  {
+    username: 'john',
+    team: 'red',
+    score: 5,
+    items: ['ball', 'book', 'pen'],
+  },
+  {
+    username: 'becky',
+    team: 'blue',
+    score: 10,
+    items: ['tape', 'backpack', 'pen'],
+  },
+  {
+    username: 'susy',
+    team: 'red',
+    score: 55,
+    items: ['ball', 'eraser', 'pen'],
+  },
+  {
+    username: 'tyson',
+    team: 'green',
+    score: 1,
+    items: ['book', 'pen'],
+  },
+];
+
+const output1 = inputThree.reduce(
+  (prev, curr) => {
+    prev[0] += curr.score;
+    return prev;
+  },
+  [0]
+);
+
+const output2 = inputThree.reduce((acc, curr) => {
+  acc.push({
+    myScore: curr.score,
+  });
+
+  return acc;
+}, []);
+
+const output3 = inputThree.reduce(
+  (prev, curr) => {
+    prev.totalScore += curr.score;
+    return prev;
+  },
+  { totalScore: 0 }
+);
+
+const output4 = inputThree.reduce(
+  (prev, curr, index) => {
+    prev.totalScore += curr.score;
+    if (index === inputThree.length - 1) {
+      prev.averageScore = prev.totalScore / inputThree.length;
+    }
+
+    return prev;
+  },
+  { totalScore: 0 }
+);
+
+const sumAverage = inputThree.reduce(
+  (acc, curr, index) => {
+    acc.totalScore = acc.totalScore + curr.score;
+    acc.avgScore = acc.totalScore / (index + 1);
+
+    return acc;
+  },
+  { totalScore: 0, avgScore: 0 }
+);
+
+console.log('output 1 = ', output1);
+console.log('output 2 = ', output2);
+console.log('output 3 = ', output3);
+console.log('output 4 = ', output4);
+console.log('output 5 = ', sumAverage);
